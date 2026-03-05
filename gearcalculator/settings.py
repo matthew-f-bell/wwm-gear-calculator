09d53e63391be47f64ef1548dff3d5812d19b964
matthew-f-bell <xdjx3runx@gmail.com>
2026-03-05 14:13:17 -0800

    Created base.html, home.html, Home view, and Stats Model

--- gearcalculator/settings.py
+++ gearcalculator/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import psycopg2
+
 import os
 
 load_dotenv()
@@ -29,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Stats(models.Model):
+    stat_name = models.CharField(max_length=100)
+    stat_value = models.FloatField()
+    max_stat_value = models.FloatField()
+    stat_weight = models.FloatField()
+
+    REQUIRED_FIELDS = ['stat_name', 'stat_value', 'max_stat_value', 'stat_weight']
+
+    def __str__(self):
+        return self.stat_name
